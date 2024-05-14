461d676f1fa42585ba11f57570f54802ec20ac16
Nikhil Dua <nikhil.dua@observeinc.com>
2024-05-13 17:41:15 -0700

    separate OPAL

--- .github/scripts/query_observe.py
+++ .github/scripts/query_observe.py
@@ -2,10 +2,11 @@ import configparser
 import json
 import re
 import logging
-import requests
-import os, sys
+import requests # type: ignore
+import os, sys, json
 import datetime
 from datetime import timezone
+import pipeline_config # type: ignore
 
 
 # Create a logger instance
@@ -194,9 +195,7 @@ def validate_azure_data(source: str, stale_checks_mins: int) -> bool:
     :return: if source is validated from current time (true, false)
     """
 
-    AZURE_DATASET_ID = os.environ.get("AZURE_DATASET_ID")
-    AZURE_COLLECTION_FUNCTION = os.environ.get("AZURE_COLLECTION_FUNCTION")
-    OBSERVE_TOKEN_ID = os.environ.get("OBSERVE_TOKEN_ID")
+    AZURE_DATASET_ID = os.environ.get("AZURE_DATASET_ID")    
     CURRENT_TIME_ISO = os.environ.get("CURRENT_TIME_ISO")
 
     # Query Start Time: Uses Terraform script finish time as query Start Time
@@ -214,47 +213,11 @@ def validate_azure_data(source: str, stale_checks_mins: int) -> bool:
 
     pipeline = ''
     if source == "EventHub":
-        # OPAL PIPELINE to execute on Dataset
-        pipeline = "make_col _ob_datastream_token_id:coalesce(DATASTREAM_TOKEN_ID, string(EXTRA.datastream_token_id))| " \
-                   "filter _ob_datastream_token_id = '{}'|" \
-                   "filter (string(EXTRA.collection_version) = '{}')|" \
-                   "filter (string(EXTRA.source) = 'EventHub')| " \
-                   "make_col timestamp:parse_timestamp(string(FIELDS.time), 'MM/DD/YYYY HH24:MI:SS')| " \
-                   "make_col timestamp:if_null(timestamp,parse_isotime(string(FIELDS.time)))| " \
-                   "set_valid_from options(max_time_diff:30m), timestamp|" \
-                   "make_col FIELDS:parse_json(string(FIELDS))|" \
-                   "make_col source: string(EXTRA.source)|" \
-                   "make_col category:string(FIELDS.category)|" \
-                   "make_col appName:string(FIELDS.properties.appName)|" \
-                   "make_col message:string(FIELDS.properties.message)|" \
-                   "make_col time_string:string(FIELDS.time)|" \
-                   "pick_col timestamp, time_string, source, category, appName, message|" \
-                   "statsby msg_count: count_distinct(message), earliest_ts: first_not_null(timestamp), group_by(source)" \
-                   "". \
-            format(OBSERVE_TOKEN_ID, AZURE_COLLECTION_FUNCTION)
+        pipeline = pipeline_config.eventhub_pipeline
     elif source == "ResourceManagement":
-        pipeline = "make_col _ob_datastream_token_id:coalesce(DATASTREAM_TOKEN_ID, string(EXTRA.datastream_token_id))| " \
-                   "filter _ob_datastream_token_id = '{}'|" \
-                   "filter (string(EXTRA.collection_version) = '{}')|" \
-                   "filter (string(EXTRA.source) = 'ResourceManagement')|" \
-                   "make_col source: string(EXTRA.source)|" \
-                   "make_col type:string(FIELDS.type)|" \
-                   "statsby msg_count: count_distinct(type), earliest_ts: first_not_null(BUNDLE_TIMESTAMP), group_by(source)" \
-                   "". \
-            format(OBSERVE_TOKEN_ID, AZURE_COLLECTION_FUNCTION)
+        pipeline = pipeline_config.resource_management_pipeline
     elif source == "VmMetrics":
-        pipeline = "make_col _ob_datastream_token_id:coalesce(DATASTREAM_TOKEN_ID, string(EXTRA.datastream_token_id))| " \
-                   "filter _ob_datastream_token_id = '{}'|" \
-                   "filter (string(EXTRA.collection_version) = '{}')|" \
-                   "filter (string(EXTRA.source) = 'VmMetrics')| " \
-                   "make_col time_string: string(FIELDS.timeseries[0].data[0].timeStamp)|" \
-                   "make_col timestamp:parse_isotime(string(FIELDS.timeseries[0].data[0].timeStamp))| " \
-                   "make_col metric_name:string(FIELDS.name.value)|" \
-                   "make_col FIELDS:parse_json(string(FIELDS))|" \
-                   "make_col source: string(EXTRA.source)|" \
-                   "statsby msg_count: count_distinct(metric_name), earliest_ts: first_not_null(BUNDLE_TIMESTAMP), group_by(source)" \
-                   "". \
-            format(OBSERVE_TOKEN_ID, AZURE_COLLECTION_FUNCTION)
+        pipeline = pipeline_config.vm_metrics_pipeline
 
     # Query Dataset with pipeline and write results to JSON
     bearer_token = get_bearer_token()
@@ -311,6 +274,7 @@ if __name__ == '__main__':
     log_level = logging.INFO
 
     setup_logger(log_level, log_format)
+
 
     logger.info("Validating Azure Data")
     logger.info("------------------------------------")
